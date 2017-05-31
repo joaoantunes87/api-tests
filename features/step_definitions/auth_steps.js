593311c6689dcf5786bbf3bd038889e8ff8957aa
joaoantunes <joaoantunes87@gmail.com>
2017-05-31 12:05:47 +0100

    Setting Up Cucumber Custom World

--- features/step_definitions/auth_steps.js
+++ features/step_definitions/auth_steps.js
@@ -17,7 +17,7 @@ defineSupportCode(function({Given, When, Then}) {
   });
 
   Then('I should be authenticated', function () {
-    return axios.get('https://play.dhis2.org/dev/api/me',
+    return axios.get(this.apiEndpoint + 'api/me',
      {
        auth: {
          username: this.username,
@@ -29,7 +29,7 @@ defineSupportCode(function({Given, When, Then}) {
   });
 
   Then('I should be not be authenticated', function () {
-    return axios.get('https://play.dhis2.org/dev/api/me',
+    return axios.get(this.apiEndpoint + 'api/me',
      {
        auth: {
          username: this.username,
--- features/support/world.js
+++ features/support/world.js
@@ -1,1 +1,9 @@
 
+var {defineSupportCode} = require('cucumber');
+function CustomWorld() {
+    this.apiEndpoint = "https://play.dhis2.org/dev/";
+}
+
+defineSupportCode(function({setWorldConstructor}) {
+  setWorldConstructor(CustomWorld);
+})
