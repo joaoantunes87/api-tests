7a93169861fe7624b1b479155007b393d7641a4c
João Antunes <joao.antunes@present-technologies.com>
2017-06-13 11:26:41 +0100

    Refactoring Organisation Unit Feature and Step Implementations

--- features/organisation_units/orgunits.feature
+++ features/organisation_units/orgunits.feature
@@ -16,9 +16,13 @@ I want to be able to add and manage organisation units
 
       Scenario: Assign a parent to an organsiation unit
         When an existing parent organisation unit exists
-        And I create a new organisation unit
+        And I fill in all of the required fields with data:
+        | name            | shortName       | openingDate |
+        | Organization 2  | ORG             | 2016-06-23  |
         Then I should be able to assign the existing organisation unit as a parent
-        And I should be informed that the organisation unit was created.
+        And I submit the organisation unit
+        Then I should be informed that the organisation unit was created
+        And The returned data is the same as submitted.
 
       Scenario Outline: Update valid properties of an organisation unit with valid values
         When I update an organisation unit
@@ -31,7 +35,7 @@ I want to be able to add and manage organisation units
         | openingDate | 1970-01-01T00:00:00.000 |
 
       Scenario Outline: Update properties of an organisation unit with invalid values
-        When I update an organisation unit
+        When I update an existing organisation unit
         And I provide an invalid <value> of a valid <property>
         Then I should receive an error message.
         #A bit unknown at this point, but expect a 500.
@@ -41,7 +45,7 @@ I want to be able to add and manage organisation units
         | openingDate | 1970-02-31T00:00:00.000 |
 
       Scenario Outline: Update non-existent properties of an organisation unit
-        When I update an organisation unit
+        When I update an existing organisation unit
         And I provide an invalid <value> of an invalid <property>
         Then I should be informed that the organisation unit was not updated.
         #Also a bit unknown but assume a 204.
@@ -56,19 +60,19 @@ I want to be able to add and manage organisation units
         Then I should be able to view the translated name.
 
       Scenario Outline: Define an end date for an organisation unit
-        When I have created an organisation unit with an opening date as <openingDate>
+        When I update an existing organisation unit
         And I provide a closed date as <pastDate>
         Then I should receive an error message.
         Examples:
-        | openingDate | pastDate |
-        | 2015-01-31T00:00:00.000 | 2015-01-30T00:00:00.000 |
-        | 2010-05-04T00:00:00.000 | 1970-02-25T00:00:00.000 |
+        | pastDate |
+        | 2015-01-30T00:00:00.000 |
+        | 1970-02-25T00:00:00.000 |
 
       Scenario Outline: Define an end date for an organisation unit
-        When I have created an organisation unit with an opening date as <openingDate>
+        When I update an existing organisation unit
         And I provide a closed date as <futureDate>
         Then I should be informed that the organisation unit was updated.
         Examples:
-        | openingDate | futureDate |
-        | 2015-01-30T00:00:00.000 | 2015-01-31T00:00:00.000 |
-        | 1970-02-25T00:00:00.000 | 2010-05-04T00:00:00.000 |
+        | futureDate |
+        | 2017-06-12T00:00:00.000 |
+        | 2017-06-13T00:00:00.000 |
--- features/step_definitions/organisation_units.js
+++ features/step_definitions/organisation_units.js
@@ -1,9 +1,135 @@
 const { defineSupportCode } = require('cucumber');
 const chai = require('chai');
-// const faker = require('faker');
 const moment = require('moment');
-
 const assert = chai.assert;
+
+defineSupportCode(function ({Before, Given, When, Then}) {
+  const organisationUnitId = generateIds();
+  let organisationUnitWasCreated = false;
+
+  Given(/^that I am logged in$/, function () {
+    this.organisationUnitData = {};
+    this.organisationUnitUpdateRequest = {};
+  });
+
+  Given(/^that I have the necessary permissions to add an organisation unit$/, function () {
+    /* Test done at authentication.feature. Basic Auth in all requests */
+  });
+
+  When(/^I fill in all of the required fields with data:$/, function (data) {
+    const properties = data.rawTable[0];
+    const values = data.rawTable[1];
+    properties.forEach(function (propertyKey, index) {
+      this.organisationUnitData[propertyKey] = values[index];
+    }, this);
+  });
+
+  When(/^I submit the organisation unit$/, function () {
+    const world = this;
+
+    // OrganisationUnit to be used for other tests not created yet. For that we want a specific id
+    if (organisationUnitWasCreated === false) {
+      world.organisationUnitData.id = organisationUnitId;
+    }
+
+    return initializeOrganisationUnitPostPromise(world, world.organisationUnitData).then(function (response) {
+      world.organisationUnitPostResponseStatus = response.status;
+      world.organisationUnitPostResponseUid = response.data.response.uid;
+    });
+  });
+
+  Then(/^I should be informed that the organisation unit was created$/, function () {
+    assert.equal(this.organisationUnitPostResponseStatus, 201, 'Status should be 201');
+    assert.isOk(this.organisationUnitPostResponseUid, 'Organisation Unit Id was not returned');
+    if (this.organisationUnitPostResponseStatus === 201 && this.organisationUnitPostResponseUid === organisationUnitId) {
+      organisationUnitWasCreated = true;
+    }
+  });
+
+  Then(/^The returned data is the same as submitted.$/, function () {
+    const world = this;
+
+    return initializeOrganisationUnitGetPromise(world, this.organisationUnitPostResponseUid).then(function (response) {
+      assert.equal(response.data.name, world.organisationUnitData.name, 'Name is wrong');
+      assert.equal(response.data.shortName, world.organisationUnitData.shortName, 'Short name is wrong');
+      assert.equal(moment(response.data.openingDate).format(world.momentDateFormat), world.organisationUnitData.openingDate, 'Opening Date is wrong');
+    });
+  });
+
+  When(/^an existing parent organisation unit exists$/, function () {
+    assert.equal(organisationUnitWasCreated, true, 'Organisation Unit does not exist');
+  });
+
+  Then(/^I should be able to assign the existing organisation unit as a parent$/, function () {
+    this.organisationUnitData.parent = {
+      id: organisationUnitId
+    };
+  });
+
+  When(/^I update an existing organisation unit$/, function () {
+    assert.equal(organisationUnitWasCreated, true, 'Organisation Unit does not exist');
+    this.organisationUnitUpdateRequest = {};
+  });
+
+  When(/^I provide a valid (.+) for a valid (.+)$/, function (value, property) {
+    this.organisationUnitUpdateRequest[property] = value;
+  });
+
+  Then(/^I should be informed that the organisation unit was updated.$/, function () {
+    const world = this;
+    return initializeOrganisationUnitPatchPromise(world,
+      organisationUnitId,
+      world.organisationUnitUpdateRequest
+    ).then(function (response) {
+      assert.equal(response.status, 200, 'Organisation Unit was updated');
+      return initializeOrganisationUnitGetPromise(world, organisationUnitId).then(function (response) {
+        const keys = Object.keys(world.organisationUnitUpdateRequest);
+        keys.forEach(function (key) {
+          assert.equal(response.data[key], world.organisationUnitUpdateRequest[key], key + ' is wrong');
+        });
+      });
+    });
+  });
+
+  When(/^I provide an invalid (.+) of a valid (.+)$/, function (value, property) {
+    this.organisationUnitUpdateRequest[property] = value;
+  });
+
+  Then(/^I should receive an error message.$/, function () {
+    return initializeOrganisationUnitPatchPromise(this,
+      organisationUnitId,
+      this.organisationUnitUpdateRequest
+    ).then(function (response) {
+      throw new Error('The request should have failed');   // it should fail
+    }).catch(function (error) {
+      // request failed
+      if (error.response) {
+        assert.equal(error.response.status, 500, 'It should have returned error status');
+        assert.equal(error.response.data.status, 'ERROR', 'It should have returned error status');
+        assert.property(error.response.data, 'message', 'It should have returned error message');
+      } else {  // error thrown at then callback
+        throw error;
+      }
+    });
+  });
+
+  When(/^I provide an invalid (.+) of an invalid (.+)$/, function (value, property) {
+    this.organisationUnitUpdateRequest[property] = value;
+  });
+
+  Then(/^I should be informed that the organisation unit was not updated.$/, function () {
+    return initializeOrganisationUnitPatchPromise(this,
+      organisationUnitId,
+      this.organisationUnitUpdateRequest
+    ).then(function (response) {
+      assert.equal(response.status, 204, 'Organisation Unit was not updated');
+    });
+  });
+
+  When(/^I provide a closed date as (.+)$/, function (closedDate) {
+    this.organisationUnitUpdateRequest.closedDate = closedDate;
+  });
+});
 
 /* just a test for now. Possible to be removed. At least to be moved to a reasuble place  */
 const generateIds = (numberOfIds) => {
@@ -19,14 +145,6 @@ const generateIds = (numberOfIds) => {
   return numberOfIds ? ids : ids[0];
 };
 
-// const initializeFakeOrganizationUnit = (world) => {
-//   return {
-//     name: faker.company.companyName(),
-//     shortName: faker.company.companySuffix(),
-//     openingDate: moment(faker.date.past()).format(world.momentDateFormat)
-//   };
-// };
-
 const initializeOrganisationUnitPostPromise = (world, organisationUnitData) => {
   return world.axios({
     method: 'post',
@@ -36,15 +154,6 @@ const initializeOrganisationUnitPostPromise = (world, organisationUnitData) => {
   });
 };
 
-// const initializeOrganisationUnitPatchPromise = (world, organisationUnitId, organisationUnitUpdateRequest) => {
-//   return world.axios({
-//     method: 'patch',
-//     url: world.apiEndpoint + '/organisationUnits/' + organisationUnitId,
-//     data: organisationUnitUpdateRequest,
-//     auth: world.authRequestObject
-//   });
-// };
-
 const initializeOrganisationUnitGetPromise = (world, organisationUnitId) => {
   return world.axios({
     method: 'get',
@@ -53,156 +162,11 @@ const initializeOrganisationUnitGetPromise = (world, organisationUnitId) => {
   });
 };
 
-defineSupportCode(function ({Before, Given, When, Then}) {
-  // Before(function () {
-  //   const world = this;
-  //
-  //   return world.axios({
-  //     method: 'get',
-  //     url: world.apiEndpoint + '/schemas/organisationUnit.json?fields=properties[fieldName],properties[propertyType],properties[required],properties[writable],properties[klass],apiEndpoint',
-  //     auth: {
-  //       username: 'admin',
-  //       password: 'district'
-  //     }
-  //   }).then(function (response) {
-  //     world.organisationUnitSchema = response.data.properties.filter((property) => {
-  //       return (property.writable && (property.klass === 'java.lang.String' ||
-  //       property.klass === 'java.lang.Boolean' ||
-  //       property.klass === 'java.util.Date' ||
-  //       property.klass === 'java.lang.Integer'));
-  //     });
-  //   });
-  // });
-
-  const organisationUnitId = generateIds();
-
-  Given(/^that I am logged in$/, function () {
-    this.organisationUnitData = {};
-    this.organisationUnitUpdateRequest = {};
+const initializeOrganisationUnitPatchPromise = (world, organisationUnitId, organisationUnitUpdateRequest) => {
+  return world.axios({
+    method: 'patch',
+    url: world.apiEndpoint + '/organisationUnits/' + organisationUnitId,
+    data: organisationUnitUpdateRequest,
+    auth: world.authRequestObject
   });
-
-  Given(/^that I have the necessary permissions to add an organisation unit$/, function () {
-
-  });
-
-  When(/^I fill in all of the required fields with data:$/, function (data) {
-    // this.organisationUnitData = initializeFakeOrganizationUnit(this);
-    const properties = data.rawTable[0];
-    const values = data.rawTable[1];
-    properties.forEach(function (propertyKey, index) {
-      this.organisationUnitData[propertyKey] = values[index];
-    }, this);
-    this.organisationUnitData.id = organisationUnitId;
-    console.log('Data to send: ' + JSON.stringify(this.organisationUnitData));
-  });
-
-  When(/^I submit the organisation unit$/, function () {
-    const world = this;
-
-    return initializeOrganisationUnitPostPromise(this, this.organisationUnitData).then(function (response) {
-      world.organisationUnitPostResponseStatus = response.status;
-      world.organisationUnitPostResponseUid = response.data.response.uid;
-    });
-  });
-
-  Then(/^I should be informed that the organisation unit was created$/, function () {
-    assert.equal(this.organisationUnitPostResponseStatus, 201, 'Status should be 201');
-    assert.equal(this.organisationUnitPostResponseUid, this.organisationUnitData.id, 'Organisation Unit Id was not returned');
-  });
-
-  Then(/^The returned data is the same as submitted.$/, function () {
-    const world = this;
-
-    return initializeOrganisationUnitGetPromise(world, this.organisationUnitData.id).then(function (response) {
-      assert.equal(response.data.name, world.organisationUnitData.name, 'Name is wrong');
-      assert.equal(response.data.shortName, world.organisationUnitData.shortName, 'Short name is wrong');
-      assert.equal(moment(response.data.openingDate).format(world.momentDateFormat), world.organisationUnitData.openingDate, 'Opening Date is wrong');
-    });
-  });
-
-  // When(/^an existing parent organisation unit exists$/, function () {
-  //   const world = this;
-  //   world.organisationUnitId = null;
-  //
-  //   return initializeOrganisationUnitPostPromise(this, initializeFakeOrganizationUnit(this)).then(function (response) {
-  //     world.organisationUnitId = response.data.response.uid;
-  //   });
-  // });
-  //
-  // When(/^I create a new organisation unit$/, function () {
-  //   this.organisationUnitData = initializeFakeOrganizationUnit(this);
-  // });
-  //
-  // When(/^I should be able to assign the existing organisation unit as a parent$/, function () {
-  //   this.organisationUnitData.parent = {
-  //     id: this.organisationUnitId
-  //   };
-  // });
-  //
-  // When(/^I update an organisation unit$/, function () {
-  //   this.organisationUnitUpdateRequest = {};
-  // });
-  //
-  // When(/^I provide a valid (.+) for a valid (.+)$/, function (value, property) {
-  //   this.organisationUnitUpdateRequest[property] = value;
-  // });
-  //
-  // Then(/^I should be informed that the organisation unit was updated.$/, function () {
-  //   const world = this;
-  //   return initializeOrganisationUnitPatchPromise(world,
-  //     organisationUnitId,
-  //     world.organisationUnitUpdateRequest
-  //   ).then(function (response) {
-  //     assert.equal(response.status, 200, 'Organisation Unit was updated');
-  //     return initializeOrganisationUnitGetPromise(world, organisationUnitId).then(function (response) {
-  //       const keys = Object.keys(world.organisationUnitUpdateRequest);
-  //       keys.forEach(function (key) {
-  //         assert.equal(response.data[key], world.organisationUnitUpdateRequest[key], key + ' is wrong');
-  //       });
-  //     });
-  //   });
-  // });
-  //
-  // When(/^I provide an invalid (.+) of a valid (.+)$/, function (value, property) {
-  //   this.organisationUnitUpdateRequest[property] = value;
-  // });
-  //
-  // Then(/^I should receive an error message.$/, function () {
-  //   return initializeOrganisationUnitPatchPromise(this,
-  //     organisationUnitId,
-  //     this.organisationUnitUpdateRequest
-  //   ).then(function (response) {
-  //     throw new Error('The request should have failed');   // it should fail
-  //   }).catch(function (error) {
-  //     // request failed
-  //     if (error.response) {
-  //       assert.equal(error.response.status, 500, 'It should have returned error status');
-  //       assert.equal(error.response.data.status, 'ERROR', 'It should have returned error status');
-  //       assert.property(error.response.data, 'message', 'It should have returned error message');
-  //     } else {  // error thrown at then callback
-  //       throw error;
-  //     }
-  //   });
-  // });
-  //
-  // When(/^I provide an invalid (.+) of an invalid (.+)$/, function (value, property) {
-  //   this.organisationUnitUpdateRequest[property] = value;
-  // });
-  //
-  // Then(/^I should be informed that the organisation unit was not updated.$/, function () {
-  //   return initializeOrganisationUnitPatchPromise(this,
-  //     organisationUnitId,
-  //     this.organisationUnitUpdateRequest
-  //   ).then(function (response) {
-  //     assert.equal(response.status, 204, 'Organisation Unit was not updated');
-  //   });
-  // });
-  //
-  // When(/^I have created an organisation unit with an opening date as (.+)$/, function (openingDate) {
-  //   this.organisationUnitUpdateRequest = {};
-  // });
-  //
-  // When(/^I provide a closed date as (.+)$/, function (closedDate) {
-  //   this.organisationUnitUpdateRequest.closedDate = closedDate;
-  // });
-});
+};
